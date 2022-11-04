c476f1880749b2673d24a5ece92b0a3f847290d5
TbotaPhantA <botakiller@mail.ru>
2022-11-04 23:07:14 +0000

    application start was refactored

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,24 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import * as util from 'util';
 
 async function bootstrap() {
+  const port = 3000;
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+  await app.listen(port, (err, address) => printStartResult(err, address));
 }
+
+const printStartResult = (err: Error, address: string): void => {
+  if (err) {
+    console.log(`Error during application start ${util.inspect(err)}`);
+  } else {
+    console.log(`Nest successfully started on ${address}`);
+  }
+};
+
 bootstrap();
