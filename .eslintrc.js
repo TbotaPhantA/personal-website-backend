fab75ecde85ba49a69ca809ecb349f6eab22ce85
TbotaPhantA <botakiller@mail.ru>
2023-01-24 19:26:01 +0300

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'max-len': ["error", { "code": 100 }]
+    'max-len': ["error", { "code": 100 }],
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
   },
 };
