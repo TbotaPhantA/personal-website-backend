9021373778bb77b9dfc4991cb668d27ff533b5ba
TbotaPhantA <botakiller@mail.ru>
2023-02-02 23:24:37 +0000

    minor refactoring

--- src/application/language/services/getLanguage.service.ts
+++ src/application/language/services/getLanguage.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable } from '@nestjs/common';
 import { LANGUAGE_REPOSITORY } from '../shared/tokens';
 import { LanguageRepository } from '../repositories/language.repository';
 import { AllLanguagesOutputDto } from '../../../domain/language/shared/dto/allLanguagesOutput.dto';
-import { LanguageOutputDto } from '../../../domain/language/shared/dto/languageOutput.dto';
 
 @Injectable()
 export class GetLanguageService {
@@ -13,6 +12,6 @@ export class GetLanguageService {
 
   public async getAll(): Promise<AllLanguagesOutputDto> {
     const languages = await this.languageRepository.findAll();
-    return { languages: languages.map((l) => LanguageOutputDto.from(l)) };
+    return AllLanguagesOutputDto.from(languages);
   }
 }
--- src/domain/language/shared/dto/allLanguagesOutput.dto.ts
+++ src/domain/language/shared/dto/allLanguagesOutput.dto.ts
@@ -1,7 +1,14 @@
 import { LanguageOutputDto } from './languageOutput.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { Language } from '../../language';
 
 export class AllLanguagesOutputDto {
   @ApiProperty({ type: [LanguageOutputDto] })
   languages: LanguageOutputDto[];
+
+  public static from(languages: Language[]): AllLanguagesOutputDto {
+    const dto = new AllLanguagesOutputDto();
+    dto.languages = languages.map((l) => LanguageOutputDto.from(l));
+    return dto;
+  }
 }
