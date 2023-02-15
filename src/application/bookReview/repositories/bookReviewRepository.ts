046ee8eb821261a3e412dd20bfbaf9d2aee93b35
TbotaPhantA <botakiller@mail.ru>
2023-02-15 21:41:19 +0000

    TODO was added.

--- src/application/bookReview/repositories/bookReviewRepository.ts
+++ src/application/bookReview/repositories/bookReviewRepository.ts
@@ -1,5 +1,11 @@
 import { BookReview } from '../../../domain/bookReview/bookReview';
 
+/**
+ * TODO
+ * - think about how to make entity fields private
+ * - save to postgreSQL
+ * - create transaction services
+ */
 export interface BookReviewRepository {
   getAll(): Promise<BookReview[]>;
   save(bookReview: BookReview): Promise<BookReview>;
