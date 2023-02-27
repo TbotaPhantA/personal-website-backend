04c40dcb6973dbd261a7892dea3cb3fe6ae55b8a
TbotaPhantA <botakiller@mail.ru>
2023-02-27 19:52:19 +0000

    environment variables declaration was added.

---
+++ environment.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      HOST: string;
+      REST_PORT: string;
+      POSTGRES_DB: string;
+      POSTGRES_HOST: string;
+      POSTGRES_USER: string;
+      POSTGRES_PASSWORD: string;
+    }
+  }
+}
+
+export {}
