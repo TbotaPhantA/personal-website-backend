629e7879636a7e9265c7888fefe7a8b1daf516a1
TbotaPhantA <botakiller@mail.ru>
2023-03-03 19:21:50 +0000

    pulling dotnet variables was fixed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LanguageModule } from './application/language/language.module';
 import { BookReviewModule } from './application/bookReview/bookReview.module';
+import { KnexModule } from './infrastructure/knex/knex.module';
+import { ConfigModule } from './infrastructure/config/config.module';
 
 @Module({
-  imports: [LanguageModule, BookReviewModule],
+  imports: [ConfigModule, LanguageModule, BookReviewModule, KnexModule],
   controllers: [],
   providers: [],
 })
--- src/infrastructure/config/config.ts
+++ src/infrastructure/config/config.ts
@@ -2,15 +2,28 @@ const throwRequired = (propName: string): never => {
   throw new Error(`Config property ${propName} is required`);
 };
 
-export default {
-  nodeEnv: process.env.NODE_ENV ?? throwRequired('NODE_ENV'),
-  host: process.env.HOST ?? throwRequired('HOST'),
-  restPort: process.env.REST_PORT ?? throwRequired('REST_PORT'),
-  postgres: {
+class Config {
+  nodeEnv = process.env.NODE_ENV ?? throwRequired('NODE_ENV');
+  host =  process.env.HOST ?? throwRequired('HOST');
+  restPort =  process.env.REST_PORT ?? throwRequired('REST_PORT');
+  postgres = {
     db: process.env.POSTGRES_DB ?? throwRequired('POSTGRES_DB'),
     host: process.env.POSTGRES_HOST ?? throwRequired('POSTGRES_HOST'),
     port: process.env.POSTGRES_PORT ?? throwRequired('POSTGRES_PORT'),
     user: process.env.POSTGRES_USER ?? throwRequired('POSTGRES_USER'),
     password: process.env.POSTGRES_PASSWORD ?? throwRequired('POSTGRES_PASSWORD'),
-  },
-} as const;
+  };
+
+  private static instance: Config;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
+
+  public static getInstance(): Config {
+    if (!this.instance) { this.instance = new Config() }
+    return this.instance;
+  }
+}
+
+const config = Config.getInstance();
+
+export default config;
