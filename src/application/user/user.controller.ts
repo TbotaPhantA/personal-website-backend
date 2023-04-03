3faf25dc6e1dc07c5b11e0e6ccf10852efaaac49
TbotaPhantA <botakiller@mail.ru>
2023-04-03 19:27:09 +0000

    minor fixes

--- src/application/user/user.controller.ts
+++ src/application/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpStatus, Post, UseFilters } from '@nestjs/common';
 import { LoginUserFormDto } from '../../domain/user/shared/dto/form/loginUserForm.dto';
-import { LoginOutsideDto } from '../../domain/user/shared/dto/output/loginOutsideDto';
+import { LoginOutputDto } from '../../domain/user/shared/dto/output/loginOutputDto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AllExceptionFilter } from '../../shared/exceptionFilters/allErrors.filter';
 
@@ -13,9 +13,9 @@ export class UserController {
   @ApiResponse({
     status: HttpStatus.OK,
     description: 'user was successfully logged in',
-    type: LoginOutsideDto,
+    type: LoginOutputDto,
   })
-  async login(@Body() dto: LoginUserFormDto): Promise<LoginOutsideDto> {
+  async login(@Body() dto: LoginUserFormDto): Promise<LoginOutputDto> {
     return {
       accessToken: 'token',
     }
--- src/domain/user/shared/dto/output/loginOutputDto.ts
+++ src/domain/user/shared/dto/output/loginOutputDto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class LoginOutsideDto {
+export class LoginOutputDto {
   @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' })
   accessToken: string;
 }
